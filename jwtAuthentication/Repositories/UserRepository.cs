ab01d56fc8ad41974bc552791086e588d05ea59d
Machado-code <matheus_machado1997@hotmail.com>
2022-09-06 08:11:11 -0300

    merge

--- jwtAuthentication/Repositories/UserRepository.cs
+++ jwtAuthentication/Repositories/UserRepository.cs
@@ -7,8 +7,8 @@ namespace jwtBearer.Repositories
         public static User Get(string username, string password)
         {
             var users = new List<User>();
-            users.Add(new User { Id = 1, Username = "goku", Password = "goku", Role = "manager" });
-            users.Add(new User { Id = 2, Username = "vegeta", Password = "vegeta", Role = "employee" });
+            users.Add(new User { Id = 1, Username = "maria", Password = "maria", Role = "manager" });
+            users.Add(new User { Id = 2, Username = "joao", Password = "joao", Role = "employee" });
             return users.Where(x => x.Username.ToLower() == username.ToLower() && x.Password == x.Password).FirstOrDefault();
         }
     }
